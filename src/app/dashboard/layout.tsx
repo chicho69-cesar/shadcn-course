f3bb91755963bbc2f2163b72afa30a9cd7b11dc3
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-05-28 12:55:50 -0600

    :sparkles: Avatar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
 
 const links = [
@@ -8,6 +9,7 @@ const links = [
   { name: "dialog", href: "dialog" },
   { name: "badge", href: "badge" },
   { name: "calendar", href: "calendar" },
+  { name: "avatar", href: "avatar" },
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export default function DashboardLayout({
@@ -101,9 +103,13 @@ export default function DashboardLayout({
 
             <div className="flex items-center">
               {/* User Avatar */}
-              <div className="bg-blue-500 text-white p-2 rounded-full w-12 h-12 flex items-center justify-center">
-                FH
-              </div>
+              <Avatar>
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+                <AvatarFallback>CV</AvatarFallback>
+              </Avatar>
             </div>
           </div>
         </div>
