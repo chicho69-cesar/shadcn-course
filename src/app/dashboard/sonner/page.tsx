64bfdccd82125af9c4f845d005f20e93c509440d
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-05-29 09:57:48 -0600

    :sparkles: Sonner and Slider

---
+++ src/app/dashboard/sonner/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
+export default function Page() {
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      <Button
+        variant="outline"
+        onClick={() =>
+          toast("Event has been created", {
+            duration: 5000,
+            position: "top-right",
+            description: `Sunday, December ${new Date().getFullYear()}, 2023 at 9:00 AM`,
+            action: {
+              label: "Eliminar",
+              onClick: () => console.log("Eliminar"),
+            },
+          })
+        }
+      >
+        Show Toast
+      </Button>
+
+      <Button
+        variant="outline"
+        onClick={() =>
+          toast.success("Event has been created", {
+            className: "text-white",
+            duration: 5000,
+            position: "top-right",
+            description: `Sunday, December ${new Date().getFullYear()}, 2023 at 9:00 AM`,
+            action: {
+              label: "Undo",
+              onClick: () => console.log("Undo"),
+            },
+          })
+        }
+      >
+        Show custom colors
+      </Button>
+      
+      <Button
+        variant="outline"
+        onClick={() =>
+          toast.info("Hello World", {
+            className: "text-white",
+            duration: 5000,
+            position: "top-right",
+            description: `Sunday, December ${new Date().getFullYear()}, 2023 at 9:00 AM`,
+            action: {
+              label: "Undo",
+              onClick: () => console.log("Undo"),
+            },
+          })
+        }
+      >
+        Show custom colors
+      </Button>
+    </div>
+  )
+}
