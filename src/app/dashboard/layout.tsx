76ed87c31cbc29d17dba610627d4139cabe8b932
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-05-30 16:07:02 -0600

    :sparkles: Add Theming

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -27,6 +27,7 @@ const links = [
   { name: "resizable", href: "resizable" },
   { name: "data table", href: "data-table" },
   { name: "form", href: "form" },
+  { name: "theme", href: "theme" },
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export default function DashboardLayout({
@@ -36,7 +37,7 @@ export default function DashboardLayout({
 }) {
   return (
     <>
-      <nav className="bg-white border-b border-gray-200 fixed z-30 w-full">
+      <nav className="bg-white dark:bg-slate-900 border-b border-gray-200 dark:border-slate-700 fixed z-30 w-full">
         <div className="px-3 py-3 lg:px-5 lg:pl-3">
           <div className="flex items-center justify-between">
             <div className="flex items-center justify-start">
@@ -44,7 +45,7 @@ export default function DashboardLayout({
                 id="toggleSidebarMobile"
                 aria-expanded="true"
                 aria-controls="sidebar"
-                className="lg:hidden mr-2 text-gray-600 hover:text-gray-900 cursor-pointer p-2 hover:bg-gray-100 focus:bg-gray-100 focus:ring-2 focus:ring-gray-100 rounded"
+                className="lg:hidden mr-2 text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-gray-100 cursor-pointer p-2 hover:bg-gray-100 dark:hover:bg-slate-800 focus:bg-gray-100 focus:ring-2 focus:ring-gray-100 rounded"
               >
                 <svg
                   id="toggleSidebarMobileHamburger"
@@ -132,21 +133,21 @@ export default function DashboardLayout({
         </div>
       </nav>
 
-      <div className="flex overflow-hidden bg-white pt-16">
+      <div className="flex overflow-hidden bg-white pt-16 dark:bg-slate-900">
         <aside
           id="sidebar"
           className="fixed hidden z-20 h-full top-0 left-0 pt-16 lg:flex flex-shrink-0 flex-col w-64 transition-width duration-75"
           aria-label="Sidebar"
         >
-          <div className="relative flex-1 flex flex-col min-h-0 borderR border-gray-200 bg-white pt-0">
+          <div className="relative flex-1 flex flex-col min-h-0 borderR border-gray-200 dark:border-slate-700 bg-white dark:bg-slate-900 pt-0">
             <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-              <div className="flex-1 px-3 bg-white divide-y space-y-1">
+              <div className="flex-1 px-3 bg-white dark:bg-slate-900 divide-y space-y-1">
                 <ul className="space-y-2 pb-2">
                   {links.map((link) => (
                     <li key={link.href}>
                       <Link
                         href={link.href}
-                        className="text-base capitalize text-gray-900 font-normal rounded-lg flex items-center p-2 hover:bg-gray-100 group"
+                        className="text-base capitalize text-gray-900 dark:text-white font-normal rounded-lg flex items-center p-2 hover:bg-gray-100 dark:hover:bg-slate-800 group"
                       >
                         <span className="ml-3">{link.name}</span>
                       </Link>
@@ -159,30 +160,30 @@ export default function DashboardLayout({
         </aside>
 
         <div
-          className="bg-gray-900 opacity-50 hidden fixed inset-0 z-10"
+          className="bg-gray-900 dark:bg-white opacity-50 hidden fixed inset-0 z-10"
           id="sidebarBackdrop"
         ></div>
 
         <div
           id="main-content"
-          className="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64"
+          className="h-full w-full bg-gray-50 dark:bg-slate-800 relative overflow-y-auto lg:ml-64"
         >
           <main>
             <div className="pt-6 px-4">
               <div className="w-full min-h-[calc(100vh-230px)]">
-                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
+                <div className="bg-white dark:bg-slate-900 shadow rounded-lg p-4 sm:p-6 xl:p-8">
                   {children}
                 </div>
               </div>
             </div>
           </main>
 
-          <footer className="bg-white md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4 print:hidden">
+          <footer className="bg-white dark:bg-slate-900 md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4 print:hidden">
             <ul className="flex items-center flex-wrap mb-6 md:mb-0">
               <li>
                 <a
                   href="#"
-                  className="text-sm font-normal text-gray-500 hover:underline mr-4 md:mr-6"
+                  className="text-sm font-normal text-gray-500 dark:text-gray-200 hover:underline mr-4 md:mr-6"
                 >
                   Terms and conditions
                 </a>
@@ -191,7 +192,7 @@ export default function DashboardLayout({
               <li>
                 <a
                   href="#"
-                  className="text-sm font-normal text-gray-500 hover:underline mr-4 md:mr-6"
+                  className="text-sm font-normal text-gray-500 dark:text-gray-200 hover:underline mr-4 md:mr-6"
                 >
                   Privacy Policy
                 </a>
@@ -200,7 +201,7 @@ export default function DashboardLayout({
               <li>
                 <a
                   href="#"
-                  className="text-sm font-normal text-gray-500 hover:underline mr-4 md:mr-6"
+                  className="text-sm font-normal text-gray-500 dark:text-gray-200 hover:underline mr-4 md:mr-6"
                 >
                   Licensing
                 </a>
@@ -209,7 +210,7 @@ export default function DashboardLayout({
               <li>
                 <a
                   href="#"
-                  className="text-sm font-normal text-gray-500 hover:underline mr-4 md:mr-6"
+                  className="text-sm font-normal text-gray-500 dark:text-gray-200 hover:underline mr-4 md:mr-6"
                 >
                   Cookie Policy
                 </a>
@@ -218,7 +219,7 @@ export default function DashboardLayout({
               <li>
                 <a
                   href="#"
-                  className="text-sm font-normal text-gray-500 hover:underline"
+                  className="text-sm font-normal text-gray-500 dark:text-gray-200 hover:underline"
                 >
                   Contact
                 </a>
@@ -226,7 +227,7 @@ export default function DashboardLayout({
             </ul>
 
             <div className="flex sm:justify-center space-x-6">
-              <a href="#" className="text-gray-500 hover:text-gray-900">
+              <a href="#" className="text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-white">
                 <svg
                   className="h-5 w-5"
                   fill="currentColor"
@@ -241,7 +242,7 @@ export default function DashboardLayout({
                 </svg>
               </a>
 
-              <a href="#" className="text-gray-500 hover:text-gray-900">
+              <a href="#" className="text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-white">
                 <svg
                   className="h-5 w-5"
                   fill="currentColor"
@@ -256,7 +257,7 @@ export default function DashboardLayout({
                 </svg>
               </a>
 
-              <a href="#" className="text-gray-500 hover:text-gray-900">
+              <a href="#" className="text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-white">
                 <svg
                   className="h-5 w-5"
                   fill="currentColor"
@@ -267,7 +268,7 @@ export default function DashboardLayout({
                 </svg>
               </a>
 
-              <a href="#" className="text-gray-500 hover:text-gray-900">
+              <a href="#" className="text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-white">
                 <svg
                   className="h-5 w-5"
                   fill="currentColor"
@@ -282,7 +283,7 @@ export default function DashboardLayout({
                 </svg>
               </a>
 
-              <a href="#" className="text-gray-500 hover:text-gray-900">
+              <a href="#" className="text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-white">
                 <svg
                   className="h-5 w-5"
                   fill="currentColor"
@@ -299,7 +300,7 @@ export default function DashboardLayout({
             </div>
           </footer>
 
-          <p className="text-center text-sm text-gray-500 my-10 print:hidden">
+          <p className="text-center text-sm text-gray-500 dark:text-gray-200 my-10 print:hidden">
             &copy; 2019-{new Date().getFullYear()}{" "}
             <a href="#" className="hover:underline" target="_blank">
               Themesberg
